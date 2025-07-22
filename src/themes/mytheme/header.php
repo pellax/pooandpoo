7373fb7ca34a301b34f7d4435f0d643c01fd3784
pellax <tuxsialive@gmail.com>
2025-07-22 22:16:56 +0200

    menú lateral añadido

--- src/themes/mytheme/header.php
+++ src/themes/mytheme/header.php
@@ -4,8 +4,10 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
+    
 </head>
 <body <?php body_class(); ?>>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-center p-0 m-0">
     <div class="container-fluid justify-content-center p-0 m-0">
         <a class="navbar-brand mx-auto d-flex flex-column align-items-center p-0 m-0" href="<?php echo home_url(); ?>">
@@ -15,3 +17,29 @@
         </a>
     </div>
 </nav>
+
+<!-- Menú lateral (offcanvas) -->
+<div class="sidebar bg-black text-white position-fixed h-100" >
+  <div class="p-3">
+    <h5>Menú</h5>
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#"><span class="diamond"></span><span class="menu-text">Inicio</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#"><span class="diamond"></span><span class="menu-text">Acerca de</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#"><span class="diamond"></span><span class="menu-text">Exposiciones</span></a>
+
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#"><span class="menu-text">Contacto</span><span class="diamond"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#"><span class="diamond"></span>Contacto</a>
+      </li>
+      <!-- Agrega más enlaces aquí -->
+    </ul>
+  </div>
+</div>
--- src/themes/mytheme/index.php
+++ src/themes/mytheme/index.php
@@ -1,8 +1,13 @@
 <?php
 get_header();
+?>
+<div id="main-content">
+<?php
 if ( have_posts() ) :
     while ( have_posts() ) : the_post();
         the_content();
     endwhile;
 endif;
-get_footer(); 
+get_footer();
+?>
+</div>
